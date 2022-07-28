582a259dff74d34500c2a8ca358a0c013f8e16e8
Bora Yüret <borayuret@gmail.com>
2022-07-28 17:24:47 +0300

    Aded props to Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,12 +3,14 @@ import { Navbar } from "react-bootstrap";
 
 import Button from "react-bootstrap/Button";
 
-const Header = () => {
+const Header = (props) => {
+  const { title } = props;
+
   return (
     <Navbar bg="light" variant="light">
-        <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+      <Navbar.Brand href="/">{title}</Navbar.Brand>
     </Navbar>
-  )
+  );
 };
 
 export default Header;
