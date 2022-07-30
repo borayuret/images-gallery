8af23f203dd170a6f08735790e254920296f8995
Bora Yüret <borayuret@gmail.com>
2022-07-30 18:29:30 +0300

    Search component added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
 
 import Button from "react-bootstrap/Button";
+
+const navbarStyle = {
+  backgroundColor: "lightblue",
+};
 
 const Header = (props) => {
   const { title } = props;
 
   return (
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="/">{title}</Navbar.Brand>
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
     </Navbar>
   );
 };
